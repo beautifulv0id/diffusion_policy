fix quaternion order

diffusion_policy/common/rlbench_util.py
@@ -591,7 +591,7 @@ def pose_gr_ic_from_obs(obs, obs_m1=None):
     gripper_pose = obs.gripper_pose
     pos = gripper_pose[:3]
     quat = gripper_pose[3:]
-    quat = normalize_quaternion(quaternion_to_real_part_first(quat))
+    quat = normalize_quaternion(quat)
     return np.concatenate([pos, quat, gripper_open, ignore_collisions])
 
 

diffusion_policy/dataset/rlbench_zarr_dataset.py
@@ -50,16 +50,21 @@ def collate_samples(camera_data, gripper_pose, gripper_open, ignore_collisions, 
     return sample
     
 def add_noise_to_gripper_pose(gripper_pose, rot_noise_scale, pos_noise_scale):
+    if rot_noise_scale == 0 and pos_noise_scale == 0:
+        return gripper_pose
+    
     t, _ = gripper_pose.shape
     gripper_pose, ret = gripper_to_se3(gripper_pose)
 
     # add noise to rotation
-    drot = normal_so3(t, scale=rot_noise_scale).to(gripper_pose.device)
-    gripper_pose[:,:3,:3] = torch.bmm(drot, gripper_pose[:,:3,:3])
+    if rot_noise_scale > 0:
+        drot = normal_so3(t, scale=rot_noise_scale).to(gripper_pose.device)
+        gripper_pose[:,:3,:3] = torch.bmm(drot, gripper_pose[:,:3,:3])
 
     # add noise to translation
-    dpos = torch.normal(0, pos_noise_scale, (t, 3)).to(gripper_pose.device)
-    gripper_pose[:, :3, 3] += dpos
+    if pos_noise_scale > 0:
+        dpos = torch.normal(0, pos_noise_scale, (t, 3)).to(gripper_pose.device)
+        gripper_pose[:, :3, 3] += dpos
 
     gripper_pose = se3_to_gripper(gripper_pose, ret)
     return gripper_pose

diffusion_policy/env_runner/rlbench_utils.py
@@ -210,9 +210,9 @@ def _evaluate_task_on_demos_multiproc(proc_num : int,
                     gt_action = gt_actions[step_id][None]
 
                 if env._recording:
-                    obs_state.append(create_obs_state_plot(obs_dict, lowdim=env.apply_low_dim_pcd, pred_action=torch.from_numpy(trajectory)[None], gt_action=gt_action)[0])
+                    obs_state.append(create_obs_state_plot(obs_dict, lowdim=env.apply_low_dim_pcd, pred_action=torch.from_numpy(trajectory)[None], gt_action=gt_action, quaternion_format='xyzw')[0])
                     if env.apply_mask:
-                        obs_state.append(create_obs_state_plot(obs_dict, use_mask=True, pred_action=torch.from_numpy(trajectory)[None], gt_action=gt_action)[0])
+                        obs_state.append(create_obs_state_plot(obs_dict, use_mask=True, pred_action=torch.from_numpy(trajectory)[None], gt_action=gt_action, quaternion_format='xyzw')[0])
                     if env.apply_mask:
                         logging_masks.append((masks[-1,-1].int() * 255).expand(3, -1, -1).cpu().numpy().astype(np.uint8))
                 