b798d9c576690ee14e18d211aaa5b2689ab3f696
Felix Herrmann <felix.rodau@gmail.com>
2024-12-21 13:54:03 +0100

    remove prints

--- diffusion_policy/model/decoder/se3_pcd_self_attn_decoder.py
+++ diffusion_policy/model/decoder/se3_pcd_self_attn_decoder.py
@@ -13,10 +13,6 @@ class SE3PCDSelfAttnDecoder(nn.Module):
     def forward(self, tgt, cross_memory, self_memory, query_geometric_args, cross_geometric_args, self_geometric_args):
         nact = tgt.size(1)
         geometric_args = {'query': query_geometric_args, 'key': cross_geometric_args}
-        print("CROSS1")
-        print("tgt", tgt.shape)
-        print("cross_memory", cross_memory.shape)
-        print_dict(geometric_args)
         out = self.cross_attn1(tgt, cross_memory, geometric_args=geometric_args)
 
         out = torch.cat([out, self_memory], dim=1)
@@ -24,19 +20,12 @@ class SE3PCDSelfAttnDecoder(nn.Module):
         vectors = torch.cat([query_geometric_args['vectors'], self_geometric_args['vectors']], dim=1)
         geometric_args = {'query': {'centers': centers, 'vectors': vectors}}
 
-        print("SELF")
-        print("out", out.shape)
-        print_dict(geometric_args)
         self_out = self.self_attn(tgt=out, geometric_args=geometric_args)
 
         tgt = self_out[:, :nact]
         cross_memory = self_out[:, nact:]
         geometric_args = {'query': {'centers': query_geometric_args['centers'][:, :nact], 'vectors': query_geometric_args['vectors'][:, :nact]},
                           'key': {'centers': geometric_args['query']['centers'][:, nact:], 'vectors': geometric_args['query']['vectors'][:, nact:]}}
-        print("CROSS2")
-        print("tgt", tgt.shape)  
-        print("cross_memory", cross_memory.shape)
-        print_dict(geometric_args)
         out = self.cross_attn2(tgt, cross_memory, geometric_args=geometric_args)
 
         return out
