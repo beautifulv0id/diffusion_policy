51dc12ed20f0201ef77f7aa25a4bf2f1fed3f1df
Niklas Funk <niklas.funk@tu-darmstadt.de>
2025-01-07 22:00:51 +0100

    fixing minor issue in slurm script

--- slurm_scripts/se3_flow/train_open_drawer_docker_no_bounds_fms21_dl.sh
+++ slurm_scripts/se3_flow/train_open_drawer_docker_no_bounds_fms21_dl.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=../../data/logs/%A_open_drawer_image/train_%a.out
 #SBATCH -J open_drawer_image
 
-training_script=train_se3_flow_matching_no_bounds_fms2_dl.py
+training_script=train_se3_flow_matching_no_bounds_fms21_dl.py
 task_name=open_drawer
 task_config=open_drawer_image
 job_name=$task_config
