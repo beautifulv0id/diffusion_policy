ca88cd2a7a61f1662ca39bcd58bb05af579edf29
felix herrmann <felix.rodau@gmail.com>
2025-01-17 06:54:12 +0000

    add dropout

--- diffusion_policy/model/decoder/se3_pcd_self_attn_decoder.py
+++ diffusion_policy/model/decoder/se3_pcd_self_attn_decoder.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 from geo3dattn.model.ursa_transformer.ursa_transformer import URSATransformerEncoder, URSATransformer
 from diffusion_policy.common.pytorch_util import print_dict
 class SE3PCDSelfAttnDecoder(nn.Module):
-    def __init__(self, embedding_dim):
+    def __init__(self, embedding_dim, dropout=0.2):
         super().__init__()
 
-        self.cross_attn1 = URSATransformer(d_model=embedding_dim, nhead=8, num_layers=4)
-        self.self_attn = URSATransformerEncoder(d_model=embedding_dim, nhead=8, num_layers=4)
-        self.cross_attn2 = URSATransformer(d_model=embedding_dim, nhead=8, num_layers=4)
+        self.cross_attn1 = URSATransformer(d_model=embedding_dim, nhead=8, num_layers=4, dropout=dropout)
+        self.self_attn = URSATransformerEncoder(d_model=embedding_dim, nhead=8, num_layers=4, dropout=dropout)
+        self.cross_attn2 = URSATransformer(d_model=embedding_dim, nhead=8, num_layers=4, dropout=dropout)
 
     def forward(self, tgt, cross_memory, self_memory, query_geometric_args, cross_geometric_args, self_geometric_args):
         nact = tgt.size(1)
@@ -19,7 +19,7 @@ class SE3PCDSelfAttnDecoder(nn.Module):
         centers = torch.cat([query_geometric_args['centers'], self_geometric_args['centers']], dim=1)
         vectors = torch.cat([query_geometric_args['vectors'], self_geometric_args['vectors']], dim=1)
         geometric_args = {'query': {'centers': centers, 'vectors': vectors}}
-
+        
         self_out = self.self_attn(tgt=out, geometric_args=geometric_args)
 
         tgt = self_out[:, :nact]
